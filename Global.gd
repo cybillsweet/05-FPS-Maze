5d5554fc47d69685e1168c6da472540401df27a6
cybillsweet <cybbanks@iu.edu>
2022-11-15 17:04:23 -0500

    continue

--- Global.gd
+++ Global.gd
@@ -1,6 +1,19 @@
 extends Node
 
+var menu = null
 
+func  _ready():
+	pause_mode = Node.PAUSE_MODE_PROCESS
+	
 func _unhandled_input(_event):
 	if Input.is_action_just_pressed("menu"):
-		get_tree().quit()
+		if menu == null:
+			menu = get_node_or_null("/root/Game/UI/Menu")
+		if menu != null:
+			if not menu.visible:
+				get_tree().paused = true
+				menu.visible = true
+			else: 
+				get_tree().paused = false
+				menu.hide()
+			
