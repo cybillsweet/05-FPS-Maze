b4a1ec8fee2b8790fd15bd8a3b6280ebb1a26e41
cybillsweet <cybbanks@iu.edu>
2022-12-07 21:23:46 -0500

    continue

--- Global.gd
+++ Global.gd
@@ -1,20 +1,17 @@
 extends Node
 
-var which_player = 0
-var player2id = -1
 var menu = null
 
 func _ready():
 	pause_mode = Node.PAUSE_MODE_PROCESS
 
-func _unhandled_input(event):
-	if Input.is_action_pressed("menu"):
+func _unhandled_input(_event):
+	if Input.is_action_just_pressed("menu"):
 		if menu == null:
 			menu = get_node_or_null("/root/Game/UI/Menu")
 		if menu != null:
 			if not menu.visible:
 				get_tree().paused = true
-				menu.visible = true
 			else:
 				get_tree().paused = false
-				menu.visible = false
+				menu.hide()
--- UI/HUD.gd
+++
@@ -1,4 +0,0 @@
-extends Control
-
-func _ready():
-	$Player.text = "Player " + str(Global.which_player)
